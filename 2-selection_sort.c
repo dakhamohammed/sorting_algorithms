7297e00d21157298434fc575d2a97bdb0ff8e5fe
dakhamohammed <isanagy@live.fr>
2023-10-24 06:55:46 -0700

    fix task2.

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,6 +12,11 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_elem, temp;
 
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min_elem = i;
