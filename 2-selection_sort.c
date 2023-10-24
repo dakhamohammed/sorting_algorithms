c1f14186a557c28ed01534488e8203e7099124db
dakhamohammed <isanagy@live.fr>
2023-10-24 08:31:24 -0700

    fix task 2 errors.

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,7 +10,8 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_elem, temp;
+	int *min_e, temp, *arr_i;
+	size_t i, j;
 
 	if (array == NULL || size < 2)
 	{
@@ -19,20 +20,22 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_elem = i;
+		min_e = array + i;
+		arr_i = array + i;
 
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_elem])
-			{
-				min_elem = j;
-			}
+			min_e = (array[j] < *min_e) ? (array + j) : min_e;
 		}
 
-		temp = array[min_elem];
-		array[min_elem] = array[i];
-		array[i] = temp;
-		print_array(array, size);
+		if ((array + i) != min_e)
+		{
+			temp = *(array + i);
+			*arr_i = *min_e;
+			*min_e = temp;
+
+			print_array(array, size);
+		}
 	}
 }
 
